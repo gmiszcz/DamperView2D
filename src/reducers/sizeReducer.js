28068217f0a885ee65f9d1d12ae6dc61174d3711
gmiszcz <patryk.batog@gmail.com>
2025-03-08 06:49:53 +0100

    Update sizeReducer.js

--- src/reducers/sizeReducer.js
+++ src/reducers/sizeReducer.js
@@ -1,1 +1,11 @@
-export default sizeReducer
+
+export const sizeReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_SIZE":
+      return { ...state, width: action.payload.width, height: action.payload.height };
+    case "SET_SCALE":
+      return { ...state, groupScale: action.payload.groupScale };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
