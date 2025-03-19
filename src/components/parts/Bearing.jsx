7eadd506ccdf163315dbe7ecaf5b20aea51d726c
kolamiki <kolamiki@gmail.com>
2025-03-19 15:37:49 +0100

    add BEARING

--- src/components/parts/Bearing.jsx
+++ src/components/parts/Bearing.jsx
@@ -1,48 +1,33 @@
 import React from "react";
-import { Line, Group } from "react-konva";
+import { Line, Group, Rect } from "react-konva";
 import { usePartsContext } from "../../context/PartsContext";
 import { useSize } from "../../context/SizeContext";
 import { GLOBAL_OFFSET } from "../../utils/constants";
 import { changeBrightness } from "../../utils/utils";
 
 const Bearing = () => {
-  const { Bearing, Positions } = usePartsContext();
+  const { BRG, Positions, Rod, RT, RG } = usePartsContext();
   const { state: size } = useSize();
 
-  const { Bearing_OD, Bearing_ID, Bearing_Height } = Bearing.state.geometry;
+  const { Bearing_TH, Bearing_H } = BRG.state.geometry;
+  const { Rod_OD } = Rod.state.geometry;
+  const { RT_TH, RT_Length } = RT.state.geometry;
+  const { RG_Height } = RG.state.geometry;
   const { Bearing_Position } = Positions.state.geometry;
-  const { color, opacity, display } = Bearing.state.properties;
+  const { color, opacity, display } = BRG.state.properties;
 
-  const outerRadius = Bearing_OD / 2;
-  const innerRadius = Bearing_ID / 2;
+  console.log("RG_Height", RG_Height)
+  const outerRadius = Rod_OD / 2  + Bearing_TH;
+  // const innerRadius = Bearing_ID / 2;
 
   const positionXOffset = size.width - GLOBAL_OFFSET.x;
   const positionYOffset = size.height - GLOBAL_OFFSET.y;
 
-  const generateBearingShapePoints = () => {
-    return [
-      [-Bearing_Position, outerRadius],
-      [-Bearing_Position - Bearing_Height, outerRadius],
-      [-Bearing_Position - Bearing_Height, -outerRadius],
-      [-Bearing_Position, -outerRadius],
-    ].flat();
-  };
-
-  const generateBearingInnerShapePoints = () => {
-    return [
-      [-Bearing_Position, innerRadius],
-      [-Bearing_Position - Bearing_Height, innerRadius],
-      [-Bearing_Position - Bearing_Height, -innerRadius],
-      [-Bearing_Position, -innerRadius],
-    ].flat();
-  };
-
   return (
     <Group x={positionXOffset} y={positionYOffset}>
       {/* Outer shape */}
-      <Line points={generateBearingShapePoints()} closed fill={color} opacity={display ? opacity : 0.1} shadowBlur={1} />
-      {/* Inner shape (cross-section) */}
-      <Line points={generateBearingInnerShapePoints()} closed fill={changeBrightness(color, 0.5)} shadowBlur={0} />
+      <Rect x ={-RT_Length + RT_TH + RG_Height-Bearing_H - Bearing_Position } y = {-outerRadius} width={Bearing_H} height = {Rod_OD + 2.0 * Bearing_TH}  fill={color} opacity = {display ? opacity : 0.1} shadowBlur = {1}/>
+      {/* <Line points={generateBearingShapePoints()} closed fill={color} opacity={display ? opacity : 0.1} shadowBlur={1} /> */}
     </Group>
   );
 };
