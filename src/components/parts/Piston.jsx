8144e5dea6fd2229ce52f90d0c5a6b8b65cbb985
kolamiki <kolamiki@gmail.com>
2025-03-19 15:38:57 +0100

    added Piston

--- src/components/parts/Piston.jsx
+++ src/components/parts/Piston.jsx
@@ -1,48 +1,43 @@
-import React from "react";
-import { Line, Group } from "react-konva";
+import React, { useEffect, useState } from "react";
+import { Line, Group, Rect } from "react-konva";
 import { usePartsContext } from "../../context/PartsContext";
 import { useSize } from "../../context/SizeContext";
 import { GLOBAL_OFFSET } from "../../utils/constants";
 import { changeBrightness } from "../../utils/utils";
 
 const Piston = () => {
-  const { Piston, Positions } = usePartsContext();
+  const { PP, Rod, PT, Positions } = usePartsContext();
   const { state: size } = useSize();
 
-  const { Piston_OD, Piston_ID, Piston_Height } = Piston.state.geometry;
-  const { Piston_Position } = Positions.state.geometry;
-  const { color, opacity, display } = Piston.state.properties;
+  const { P_Length,  } = PP.state.geometry;
+  const { PT_ID } = PT.state.geometry;
+  const { Rod_Length, Rod_CurrentPosition } = Rod.state.geometry
+  const { P_Position, DL, EL, CL } = Positions.state.geometry;
+  const { color, opacity, display } = PP.state.properties;
 
-  const outerRadius = Piston_OD / 2;
-  const innerRadius = Piston_ID / 2;
+  const [pistonPosition, setPistonPosition] = useState((DL -Rod_Length))
+
+  const outerRadius = PT_ID / 2;
+  // const innerRadius = Piston_ID / 2;
 
   const positionXOffset = size.width - GLOBAL_OFFSET.x;
   const positionYOffset = size.height - GLOBAL_OFFSET.y;
 
-  const generatePistonShapePoints = () => {
-    return [
-      [-Piston_Position, outerRadius],
-      [-Piston_Position - Piston_Height, outerRadius],
-      [-Piston_Position - Piston_Height, -outerRadius],
-      [-Piston_Position, -outerRadius],
-    ].flat();
-  };
-
-  const generatePistonInnerShapePoints = () => {
-    return [
-      [-Piston_Position, innerRadius],
-      [-Piston_Position - Piston_Height, innerRadius],
-      [-Piston_Position - Piston_Height, -innerRadius],
-      [-Piston_Position, -innerRadius],
-    ].flat();
-  };
-
+    useEffect(() => {
+      const position = Rod.state.geometry.Rod_CurrentPosition;
+      if (position === "DL") {
+        setPistonPosition(DL - Rod_Length - P_Position);
+      } else if (position === "CL") {
+        setPistonPosition(CL - Rod_Length - P_Position);
+      } else if (position === "EL") {
+        setPistonPosition(EL - Rod_Length - P_Position);
+      }
+  }, [Rod.state.geometry.Rod_CurrentPosition, DL, CL, EL, Rod_Length]);
+  
   return (
     <Group x={positionXOffset} y={positionYOffset}>
       {/* Outer shape */}
-      <Line points={generatePistonShapePoints()} closed fill={color} opacity={display ? opacity : 0.1} shadowBlur={1} />
-      {/* Inner shape (cross-section) */}
-      <Line points={generatePistonInnerShapePoints()} closed fill={changeBrightness(color, 0.5)} shadowBlur={0} />
+      <Rect x={-pistonPosition} y = {-outerRadius} width={P_Length} height={PT_ID} fill={"#808080"} />
     </Group>
   );
 };
