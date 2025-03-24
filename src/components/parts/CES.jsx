ff3a81a687dd63020b367e1fc23ecec0903479d9
kolamiki <kolamiki@gmail.com>
2025-03-24 14:56:06 +0100

    refactor CES component; update geometry calculations and integrate weld point generation

--- src/components/parts/CES.jsx
+++ src/components/parts/CES.jsx
@@ -1,48 +1,91 @@
 import React from "react";
-import { Line, Group } from "react-konva";
+import { Line, Group, Rect } from "react-konva";
 import { usePartsContext } from "../../context/PartsContext";
 import { useSize } from "../../context/SizeContext";
 import { GLOBAL_OFFSET } from "../../utils/constants";
 import { changeBrightness } from "../../utils/utils";
 
 const CES = ({}) => {
-  const { CES, Positions } = usePartsContext();
+  const { CVSAe, RT, Positions } = usePartsContext();
   const { state: size } = useSize();
 
-  const { CES_OD, CES_ID, CES_Height } = CES.state.geometry;
-  const { CES_Position } = Positions.state.geometry;
-  const { color, opacity, display } = CES.state.properties;
+  const { CVSAe_HousingDiam, CVSAe_HousingTH, CVSAe_HousingHeight } = CVSAe.state.geometry;
+  const { CVSAe_ValvePosition } = Positions.state.geometry;
 
-  const outerRadius = CES_OD / 2;
-  const innerRadius = CES_ID / 2;
+  const {RT_OD1, RT_TH} = RT.state.geometry;
+
+  const { color, opacity, display } = CVSAe.state.properties;
+
+  // Calculate CES additional parameters
+  // Weld size is 1.1 times the thickness of the RT, where 1.1 is a Tomek's factor
+  const weldSize = 1.1 * RT_TH; 
+
+
 
   const positionXOffset = size.width - GLOBAL_OFFSET.x;
   const positionYOffset = size.height - GLOBAL_OFFSET.y;
 
-  const generateCESShapePoints = () => {
-    return [
-      [-CES_Position, outerRadius],
-      [-CES_Position - CES_Height, outerRadius],
-      [-CES_Position - CES_Height, -outerRadius],
-      [-CES_Position, -outerRadius],
-    ].flat();
-  };
+  // const generateCESShapePoints = () => {
+  //   return [
+  //     [-CES_Position, outerRadius],
+  //     [-CES_Position - CES_Height, outerRadius],
+  //     [-CES_Position - CES_Height, -outerRadius],
+  //     [-CES_Position, -outerRadius],
+  //   ].flat();
+  // };
 
-  const generateCESInnerShapePoints = () => {
-    return [
-      [-CES_Position, innerRadius],
-      [-CES_Position - CES_Height, innerRadius],
-      [-CES_Position - CES_Height, -innerRadius],
-      [-CES_Position, -innerRadius],
-    ].flat();
-  };
+  // const generateCESInnerShapePoints = () => {
+  //   return [
+  //     [-CES_Position, innerRadius],
+  //     [-CES_Position - CES_Height, innerRadius],
+  //     [-CES_Position - CES_Height, -innerRadius],
+  //     [-CES_Position, -innerRadius],
+  //   ].flat();
+  // };
+
+  const check_RT_OD_in_specified_position = (position) => {
+
+  }
+
+  const generate_CES_weld_points = (leftOrRightWeld) => { 
+    // This function will generate the points for the welds on the CES
+    const a = (2.0 * weldSize) / Math.sqrt(2.0);
+
+
+    let cesWeldPoints =[]
+    if (leftOrRightWeld === "right") {
+      cesWeldPoints = [
+        [0.0, 0.0], //0
+        [a, 0.0], //1
+        [0.0, -a], //2
+      ];
+    }
+    else {
+      cesWeldPoints = [
+        [0.0, 0.0], //0
+        [0.0, -a], //1
+        [-a, 0.0], //2
+      ];
+    }
+      
+
+    return cesWeldPoints.flat();
+  }
+
 
   return (
-    <Group x={positionXOffset} y={positionYOffset}>
-      {/* Outer shape */}
-      <Line points={generateCESShapePoints()} closed fill={color} opacity={display ? opacity : 0.1} shadowBlur={1} />
+    <Group x={positionXOffset} y={positionYOffset} >
+      {/* CES Outer shape */}
+      <Rect x = {-CVSAe_ValvePosition - CVSAe_HousingDiam/2.0} y = {-RT_OD1/2.0-CVSAe_HousingHeight} width={CVSAe_HousingDiam} height={CVSAe_HousingHeight} fill={color} opacity={display ? opacity : 0.1} shadowBlur={1}/>
+      
       {/* Inner shape (cross-section) */}
-      <Line points={generateCESInnerShapePoints()} closed fill={changeBrightness(color, 0.5)} shadowBlur={0} />
+      <Rect x = {-CVSAe_ValvePosition - CVSAe_HousingDiam/2.0 + CVSAe_HousingTH} y = {-RT_OD1/2.0-CVSAe_HousingHeight} width = {CVSAe_HousingDiam - 2*CVSAe_HousingTH} height={CVSAe_HousingHeight} fill={changeBrightness(color, 0.5)} opacity={display ? opacity : 0.1}/>
+      
+      {/* Right weld */}
+      <Line x={-CVSAe_ValvePosition + CVSAe_HousingDiam - CVSAe_HousingDiam/2.0} y={-RT_OD1 / 2.0} points={generate_CES_weld_points("right")} closed fill="#ffc50e" opacity={display ? opacity : 0.1} shadowBlur={1} />
+      {/* Left weld */}
+      <Line x = {-CVSAe_ValvePosition - CVSAe_HousingDiam/2.0} y = {-RT_OD1/2.0} points={generate_CES_weld_points("left")} closed fill="#ffc50e" opacity={display ? opacity : 0.1} shadowBlur={1} />
+      
     </Group>
   );
 };
